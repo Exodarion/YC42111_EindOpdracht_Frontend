f048c6d5bdd48ae7e781be94c6ae95178a9f0239
Exodarion <ExoEmail>
2021-12-23 17:21:04 +0100

    Removing of groups and candidates now works properly.

--- administrator.js
+++ administrator.js
@@ -1,36 +1,159 @@
-const partijInvoer = document.getElementById("partijInvoer");
-const kandidaatInvoer = document.getElementById("kandidaatInvoer");
+//Holds all the windows that can be displayed in the administrator menu
+var windows = 
+{ 
+    "1PartyAdd" : document.getElementById("partijInvoer"),
+    "2CandidateAdd" : document.getElementById("kandidaatInvoer"),
+    "3Partyremove" : document.getElementById("partijverwijderInvoer"),
+    "4Candidateremove" : document.getElementById("kandidaatverwijderInvoer")
+}
 var partijData;
+var candidateData;
 
-//Set default to none AKA hidden for both Add candidate and party windows
-kandidaatInvoer.style.display = 'none';
-partijInvoer.style.display = 'none';
+//Hide all windows by default
+hideWindows();
+refreshGetData();
 
-//Show Add Party Window
-function showPartijInvoer() {
-    kandidaatInvoer.style.display = 'none';
-    partijInvoer.style.display = 'block';
+///////////////////////////////////////////////////////////////////////////////////////
+// UTILITY METHODS
+///////////////////////////////////////////////////////////////////////////////////////
+
+//Hide All Windows
+function hideWindows () 
+{
+    for(let element in windows)
+        windows[element].style.display = 'none';
 }
 
-//Show Add Candidate Window
-function showKandidaatInvoer() {
-    partijInvoer.style.display = 'none';
-    kandidaatInvoer.style.display = 'block';
+//Show window specified by the 'indicator'
+function showWindow (indicator) 
+{
+    for(let element in windows)
+    {
+        let object = windows[element];
+        if(element.startsWith(indicator))
+            object.style.display = 'block';
+        else
+            object.style.display = 'none';
+    }
+}
+
+function refreshGetData () {
+    getallCandidates();
+    getPoliticalGroups();
 }
 
 //Dynamically set selection names to the party names provided
-function setPoliticalGroupNames()
+function setPoliticalGroupNames(object)
 {
     //get reference to the selection element
-    let partijSelectie = document.getElementById("partijSelectie");
+    let selectie = document.getElementById(object);
+    selectie.innerHTML = ""; //On Refresh, needs to be reset to empty
     for(i = 0; i < partijData.length; i++)
     {
         //manually add new html element inside the select section
-        partijSelectie.insertAdjacentHTML
+        selectie.insertAdjacentHTML
         ('beforeend', 
         '<option value="' + i + '" >' + partijData[i].name + '</option>');
     }
 }
+
+//Dynamically set selection names to the party names provided
+function setCandidateNames(object)
+{
+    //get reference to the selection element
+    let selectie = document.getElementById(object);
+    selectie.innerHTML = ""; //On Refresh, needs to be reset to empty
+    for(i = 0; i < candidateData.length; i++)
+    {
+        //manually add new html element inside the select section
+        selectie.insertAdjacentHTML
+        ('beforeend', 
+        '<option value="' + i + '" >' + candidateData[i].firstName + " " + 
+        candidateData[i].lastName + " : " +
+        candidateData[i].politicalGroupName +
+        '</option>');
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+// DELETE REQUESTS
+///////////////////////////////////////////////////////////////////////////////////////
+
+//Send a post request to the backend given set data
+async function sendDeleteRequest(object, DataUrl, dataObject){
+    //get local or network database URL first
+    let res = await fetch('url.json');
+    let data = await res.json();
+
+    let selectie = document.getElementById(object);
+    let url = data.link + DataUrl + dataObject[selectie.value].id;
+
+    await fetch(url, {method : 'Delete'});
+    refreshGetData(); //Refresh data after update
+} 
+
+//Manually delete a candidate from a political group without refreshing
+async function deleteCandidateFromParty(arrayID)
+{
+    //get local or network database URL first
+    let res = await fetch('url.json');
+    let data = await res.json();
+    let url = data.link + "candidate/remove/" + candidateData[arrayID].id;
+
+    await fetch(url, {method : 'Delete'});
+}
+
+function removeCandidate() 
+{
+    sendDeleteRequest("kandidaatSelectie", "candidate/remove/", candidateData);
+}
+
+function removePG() 
+{
+    //First remove all candidates from this group before removing the group
+    //If not done, the candidates that are associated with this group will have a broken link
+    let selectie = document.getElementById("partijSelectie2");
+    let partyID = partijData[selectie.value].id;
+    for(let i = 0; i < candidateData.length; i++)
+    {
+        let candidatePartyID = candidateData[i].partyID;
+        console.log("candidatePartyID " + candidatePartyID);
+        console.log("partyID " + partyID);
+        if(candidatePartyID == partyID)
+            deleteCandidateFromParty(i);
+    }
+
+    sendDeleteRequest("partijSelectie2", "politicalGroup/remove/", partijData);
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+// POST REQUESTS
+///////////////////////////////////////////////////////////////////////////////////////
+
+//Send a post request to the backend given set data
+async function sendPostRequest(JsonData, endpointURL, refData){
+    //get local or network database URL first
+    const xhr = new XMLHttpRequest();
+    let res = await fetch('url.json');
+    let data = await res.json();
+
+    xhr.open('POST', data.link + endpointURL);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(JsonData));
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                successMessage.style.display = 'block';
+                //empty the content of all ref elements after execution
+                for(let element in refData)
+                    refData[element].value = '';
+                
+                refreshGetData(); //Refresh data after update
+            }
+        }
+    }
+} 
 
 function addCandidate() 
 {
@@ -47,6 +170,7 @@ function addCandidate()
     if(refData.firstnameData.value.length < 1
     || refData.lastnameData.value.length < 1
     || refData.DOBData.value == ""
+    || refData.pgData.value == ""
     || refData.expertiseData.value.length < 1)
     { console.log("Return here plx"); return; }
 
@@ -82,28 +206,9 @@ function addPoliticalGroup() {
     sendPostRequest(json, 'politicalGroup/add', refData)
 }
 
-//Send a post request to the backend given set data
-async function sendPostRequest(JsonData, endpointURL, refData){
-    //get local or network database URL first
-    const xhr = new XMLHttpRequest();
-    let res = await fetch('url.json');
-    let data = await res.json();
-
-    xhr.open('POST', data.link + endpointURL);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify(JsonData));
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            if (xhr.status === 200) {
-                successMessage.style.display = 'block';
-                //empty the content of all ref elements after execution
-                for(let element in refData)
-                    refData[element].value = '';
-            }
-        }
-    }
-} 
+///////////////////////////////////////////////////////////////////////////////////////
+// GET REQUESTS
+///////////////////////////////////////////////////////////////////////////////////////
 
 async function getPoliticalGroups(){
     let res = await fetch('url.json');
@@ -114,11 +219,22 @@ async function getPoliticalGroups(){
     request.onreadystatechange = function(){
         if(this.readyState == 4 ){
             partijData = JSON.parse(this.responseText);
-            setPoliticalGroupNames();
+            setPoliticalGroupNames("partijSelectie");
+            setPoliticalGroupNames("partijSelectie2");
         }
     }
     request.open("GET", url);
     request.send();
 }
 
-getPoliticalGroups();
+async function getallCandidates(){
+    let res = await fetch('url.json');
+    let data = await res.json();
+    let url = data.link + "candidate/list";
+
+    res = await fetch(url);
+    candidateData = await res.json();
+    console.log(candidateData);
+
+    setCandidateNames("kandidaatSelectie");
+}
